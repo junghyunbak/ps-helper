2a8cedc54aabfa25e204ff88add0688089449caf
ppwag <jeong57281@naver.com>
2021-04-19 12:19:14 +0900

    link dll

--- 환경변수등록(관리자권한실행).cpp
+++ 환경변수등록(관리자권한실행).cpp
@@ -1,15 +1,15 @@
+#pragma comment(dll, "./bin/libmingwex-0.dll")
 #include <iostream>
 #include <unistd.h>
 #include <string>
 #define MAX_SIZE 1024
 using namespace std;
 
-
 int main(){
 	char buf[MAX_SIZE];
 	getcwd(buf, MAX_SIZE);
 	string path(buf);
-	string fullPath = "setx path \"\%path\%" + path + "\\bin;\" -m";
+	string fullPath = "setx path \"\%path\%;" + path + "\\bin\" -m";
 	system(fullPath.c_str());
 	return 0;
 }
